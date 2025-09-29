38943bb62f314a442f2415490a1ba5744aec232e
polykorablevaevgenevna-arch <poly.korableva.evgenevna@gmail.com>
2025-09-29 22:30:46 +0300

    Data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,16 +1,38 @@
-#скачивание файла из MiniO с помощью Anaconda prompt
-curl -k -o GSE13159_RAW.tar "https://94.124.179.195:9000/poly-bio-data/GSE13159_RAW.tar"
+import urllib3
+from minio import Minio
+from tqdm import tqdm
 
-#разархивирование файла с помощью программы BreeZip
+# Отключение проверки SSL (не рекомендуется для продуктивной среды)
+http_client = urllib3.PoolManager(
+    cert_reqs='CERT_NONE',  # Игнорирование SSL-сертификатов
+    timeout=urllib3.Timeout(connect=5.0, read=10.0)  # Устанавливаем тайм-ауты для соединения и чтения
+)
 
-#В языке R преобразование файлов из формата .CELL в формат .csv с помощью скрипта (он также показывает первые 6 записей в таблице:
-library(affy)
-cel_dir <- "C:\\data_eng\\GSE13159_RAW\\GSM329727.CEL"
-data <- ReadAffy(celfile.path = cel_dir)
-eset <- rma(data)
-exprs_data <- exprs(eset)
-head(exprs_data)
+# Создаем клиента Minio
+client = Minio(
+    "94.124.179.195:9001",
+    access_key="",    # Вставьте ваш access_key
+    secret_key="",    # Вставьте ваш secret_key
+    secure=True,
+    http_client=http_client
+)
 
-#В Python с помозью этого скрипта выводила первые 10 строчек
-raw_data = pd.read_csv("C:\data_eng\expression_data.csv")
-raw_data.head(10) 
+bucket_name = "poly-bio-data"
+object_name = "frequent-mutations.2025-09-28.tsv"
+file_path = r"C:\data_eng\RAW.tsv"  # Укажите правильный путь
+
+try:
+    # Получаем информацию о файле
+    stat = client.stat_object(bucket_name, object_name)
+    total_size = stat.size
+
+    # Загружаем файл с прогресс-баром
+    with client.get_object(bucket_name, object_name) as response, open(file_path, 'wb') as file, tqdm(
+        total=total_size, unit='B', unit_scale=True, desc='Downloading'
+    ) as pbar:
+        for data in response.stream(1024 * 1024):  # по 1 МБ
+            file.write(data)
+            pbar.update(len(data))
+    print("Object downloaded successfully.")
+except Exception as err:
+    print(f"Error downloading object: {err}")
