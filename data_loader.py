092f4fd229541439aa067bc391eeecf87977053e
polykorablevaevgenevna-arch <poly.korableva.evgenevna@gmail.com>
2025-10-24 14:04:47 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -42,7 +42,31 @@ except Exception as err:
 #Привидение типов
 import pandas as pd
 df = pd.read_csv('C:/data_eng/frequent-mutations.2025-09-28.tsv', sep='\t') #чтение файла
-print(df.dtypes) #вывести исходные типы данных
-newdf = df.convert_dtypes() #конвертировать в новые опитмальные типы данных
-print(newdf.head()) #вывести новые типы данных
-df.to_parquet('data.parquet', engine='pyarrow') #сохранить файл в формате .parquet
+print("Первые три строки:")
+print(df.head(3)) #вывод первых трёх строк
+print()
+print("Типы данных до преобразования:")
+print(df.dtypes) #вывод типов данных до преобразования
+print()
+
+#преобразование типов данных
+df['ssm_id'] = df['ssm_id'].astype("string")
+df['dna_change'] = df['dna_change'].astype("string")
+df['protein_change'] = df['protein_change'].astype("string")
+df['type'] = df['type'].astype("string")
+df['consequence'] = df['consequence'].astype("string")
+df['num_ssm_affected_cases'] = df['num_ssm_affected_cases'].astype("int")
+df['num_TP53_cases'] = df['num_TP53_cases'].astype("int")
+df['ssm_affected_cases_percentage'] = df['ssm_affected_cases_percentage'].astype("float")
+df['num_gdc_ssm_affected_cases'] = df['num_gdc_ssm_affected_cases'].astype("int")
+df['num_gdc_ssm_cases'] = df['num_gdc_ssm_cases'].astype("int")
+df['ssm_affected_cases_percentage'] = df['ssm_affected_cases_percentage'].astype("float")
+df['vep_impact'] = df['vep_impact'].astype("string")
+df['sift_impact'] = df['sift_impact'].astype("string")
+df['sift_score'] = df['sift_score'].astype("float")
+df['polyphen_impact'] = df['polyphen_impact'].astype("string")
+df['polyphen_score'] = df['polyphen_score'].astype("float")
+
+print("Типы данных после преобразования:")
+print(df.dtypes) #вывод типов данных после преобразования
+df.to_parquet('data.parquet', engine='pyarrow') #сохранение в соотвествующий формат
