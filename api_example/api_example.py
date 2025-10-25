a913112b943c7d84554680a45238c8331eab3fc5
polykorablevaevgenevna-arch <poly.korableva.evgenevna@gmail.com>
2025-10-25 11:54:16 +0300

    Update api_example.py

--- api_example/api_example.py
+++ api_example/api_example.py
@@ -1,14 +1,11 @@
 import requests
-import pandas as pd
 
 url = 'https://official-joke-api.appspot.com/jokes/programming/ten'
-
-#Запрос к API
 response = requests.get(url)
-data = response.json()  # data — список словарей
 
-#Создаём DataFrame
-df = pd.DataFrame(data)
-
-# Выводим DataFrame
-print(df)
+if response.status_code == 200:
+    data = response.json()
+    print(data[0]['setup'])
+    print(data[0]['punchline'])
+else:
+    print("Ошибка:", response.status_code)
